ba6a96a59c0b4fdd16ab23439fdfe8454001722d
Camilo Sanchez <camilin87@gmail.com>
2013-05-17 20:07:24 -0400

    first button event click

--- tk.py
+++ tk.py
@@ -3,6 +3,17 @@
 import sys
 import Tkinter
 
-root = Tkinter.Tk()
 
-root.mainloop()
+def button_1_click():
+    print 'lolo'
+
+
+main_form = Tkinter.Tk()
+
+button_1 = Tkinter.Button(main_form, text='Send Data', command=button_1_click)
+button_1.pack()
+
+label_1 = Tkinter.Label(main_form, text='This sends us to the moon')
+label_1.pack()
+
+main_form.mainloop()

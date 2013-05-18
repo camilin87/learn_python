a9d67914bc2c3f289de50f6164d6e4f2bc9403e1
Camilo Sanchez <camilin87@gmail.com>
2013-05-18 10:43:53 -0400

    stdin.deadline is faster

--- codechef/INTEST/intest.py
+++ codechef/INTEST/intest.py
@@ -1,4 +1,6 @@
-h = raw_input()
+import sys
+
+h = sys.stdin.readline()
 hl = h.split()
 
 count = int(hl[0])
@@ -6,9 +8,15 @@ div = int(hl[1])
 
 result = 0
 
+'''
 for i in range(0, count):
-    n = int(raw_input())
+    n = int(sys.stdin.readline())
     if n % div == 0:
+        result += 1
+'''
+
+for line in sys.stdin.readlines():
+    if int(line) % div == 0:
         result += 1
 
 print result

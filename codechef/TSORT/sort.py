42c47def4a2078f9224e425ddea10543fd375629
Camilo Sanchez <camilin87@gmail.com>
2013-05-18 21:57:59 -0400

    I removed the slow sort

--- codechef/TSORT/sort.py
+++ codechef/TSORT/sort.py
@@ -2,13 +2,12 @@ import sys
 
 count = int(sys.stdin.readline())
 
-'''
 for n in sorted(map(int, sys.stdin.readlines())):
     print n
 
 '''
-
 l_num = map(int, sys.stdin.readlines())
 l_num.sort()
 for n in l_num:
     print n
+'''

c290b7a0db6ae984f440822a6823171aae4cd554
Camilo Sanchez <camilin87@gmail.com>
2013-05-18 22:45:29 -0400

    just by calling everything within a function it runs faster

--- codechef/TSORT/sort_descaro.py
+++ codechef/TSORT/sort_descaro.py
@@ -1,21 +1,26 @@
 import sys
 
-count = int(sys.stdin.readline())
 
-big_list = [0]*1000000
+def f():
+    #only by creating this function it will run faster
+    count = int(sys.stdin.readline())
 
-for x in map(int, sys.stdin.readlines()):
-    big_list[x] += 1
+    big_list = [0]*1000000
 
-#hmmm... very intersting
-#xrange is lazy and doesn't create a list
-for i in xrange(1000000):
-    if big_list[i] > 0:
-        print (str(i) + '\n') * big_list[i]
+    for x in map(int, sys.stdin.readlines()):
+        big_list[x] += 1
 
-#even though this works, it is actually slower :(
-'''
-for i in xrange(1000000):
-    for n in xrange(big_list[i]):
-        print i
-'''
+    #hmmm... very intersting
+    #xrange is lazy and doesn't create a list
+    for i in xrange(1000000):
+        if big_list[i] > 0:
+            print (str(i) + '\n') * big_list[i]
+
+    #even though this works, it is actually slower :(
+    '''
+    for i in xrange(1000000):
+        for n in xrange(big_list[i]):
+            print i
+    '''
+
+f()

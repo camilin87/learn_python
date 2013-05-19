de24ee25e1c9311200b7ebdcee40ea97fd62bd8e
Camilo Sanchez <camilin87@gmail.com>
2013-05-18 21:57:14 -0400

    this turned out to be more slow

--- codechef/TSORT/sort.py
+++ codechef/TSORT/sort.py
@@ -2,5 +2,13 @@ import sys
 
 count = int(sys.stdin.readline())
 
+'''
 for n in sorted(map(int, sys.stdin.readlines())):
+    print n
+
+'''
+
+l_num = map(int, sys.stdin.readlines())
+l_num.sort()
+for n in l_num:
     print n

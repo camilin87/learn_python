0e29ba02365fbbd339093949efa5f38cefb0955f
Camilo Sanchez <camilin87@gmail.com>
2013-05-18 22:38:17 -0400

    range is fast

---
+++ codechef/TSORT/sort_descaro.py
@@ -0,0 +1,21 @@
+import sys
+
+count = int(sys.stdin.readline())
+
+big_list = [0]*1000000
+
+for x in map(int, sys.stdin.readlines()):
+    big_list[x] += 1
+
+#hmmm... very intersting
+#xrange is lazy and doesn't create a list
+for i in xrange(1000000):
+    if big_list[i] > 0:
+        print (str(i) + '\n') * big_list[i]
+
+#even though this works, it is actually slower :(
+'''
+for i in xrange(1000000):
+    for n in xrange(big_list[i]):
+        print i
+'''

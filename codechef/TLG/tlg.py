297ba812828b29d2feefb84171da63aa3a188180
Camilo Sanchez <camilin87@gmail.com>
2013-05-20 14:51:59 -0400

    tlg completed

--- codechef/TLG/tlg.py
+++ codechef/TLG/tlg.py
@@ -1,13 +1,24 @@
 import sys
-import math
 
-count = sys.stdin.readline()
+def main(args):
 
-max_diff = 0
+    count = sys.stdin.readline()
+    max_diff = 0
 
-for round_str in sys.stdin.readlines():
-    round_values = map(int, round_str.split())
-    player_1 = round_values[0]
-    player_2 = round_values[1]
+    for round_str in sys.stdin.readlines():
+        round_values = map(int, round_str.split())
+        player_1 = round_values[0]
+        player_2 = round_values[1]
 
-    print player_1, player_2
+        temp_max_diff = player_1 - player_2
+        if abs(temp_max_diff) > abs(max_diff):
+            max_diff = temp_max_diff
+
+
+    if max_diff > 0:
+        print 1, max_diff
+    else:
+        print 2, -max_diff
+
+if __name__=='__main__':
+    main(sys.argv)

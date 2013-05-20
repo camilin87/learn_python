7355c96c1e0fa0d6ed0fd6591f4575fa4b1ce24f
Camilo Sanchez <camilin87@gmail.com>
2013-05-20 18:07:14 -0400

    rpn completed

--- codechef/RPN/rpn.py
+++ codechef/RPN/rpn.py
@@ -1,13 +1,26 @@
 import sys
 
-l_operators = '()+-*/'
+l_operators = '()+-*/^'
 
 def rpn(expr):
     op_stack = []
     result = []
     for c in expr:
         if c in l_operators:
+            if c == ')':
+                while len(op_stack) > 0:
+                    op = op_stack.pop()
+                    if op == '(':
+                        break
+                    else:
+                        result.append(op)
+            else:
+                op_stack.append(c)
+        else:
             result.append(c)
+
+    while len(op_stack) > 0:
+        result.append(op_stack.pop())
 
     return ''.join(result)
 

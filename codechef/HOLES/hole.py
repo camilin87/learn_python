35fb776a4e49bc0a8afb0345bcaaa64dd2275915
Camilo Sanchez <camilin87@gmail.com>
2013-05-20 13:45:23 -0400

    hole completed

--- codechef/HOLES/hole.py
+++ codechef/HOLES/hole.py
@@ -1,3 +1,5 @@
+import sys 
+
 l_holes = {
     'A': 1,
     'B': 2,
@@ -7,3 +9,21 @@ l_holes = {
     'Q': 1,
     'R': 1
 }
+
+def holes_in_string(s):
+    result = 0
+    for c in s:
+        if l_holes.has_key(c):
+            result += l_holes[c]
+    return result
+
+
+def main(args):
+    count = sys.stdin.readline()
+
+    for line in sys.stdin.readlines():
+        print holes_in_string(line)
+
+
+if __name__ == '__main__':
+    main(sys.argv)

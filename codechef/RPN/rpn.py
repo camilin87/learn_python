3282c1126bc91ce69e820d1f5af8b2727268b5b1
Camilo Sanchez <camilin87@gmail.com>
2013-05-20 18:17:58 -0400

    no whiles

--- codechef/RPN/rpn.py
+++ codechef/RPN/rpn.py
@@ -4,25 +4,24 @@ l_operators = '()+-*/^'
 
 def rpn(expr):
     op_stack = []
-    result = []
+    result = ''
     for c in expr:
         if c in l_operators:
             if c == ')':
-                while len(op_stack) > 0:
+                #while len(op_stack) > 0:
+                for i in range(len(op_stack)):
                     op = op_stack.pop()
                     if op == '(':
                         break
                     else:
-                        result.append(op)
+                        result += op
             else:
                 op_stack.append(c)
         else:
-            result.append(c)
+            result += c
 
-    while len(op_stack) > 0:
-        result.append(op_stack.pop())
-
-    return ''.join(result)
+    op_stack.reverse()
+    return result + ''.join(op_stack)
 
 
 def main(args):
